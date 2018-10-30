63b42f49855988d1af01f1986a9b9bd303fe2c94
Ville Pellosniemi <villeope@metropolia.fi>
2018-10-30 12:39:55 +0200

    update

--- js5/t1/teht1.js
+++ js5/t1/teht1.js
@@ -1,1 +1,5 @@
-document.getElementById('tiedot').innerHTML = '<td class="etunimi">Ville</td>';
+document.getElementById('etunimi').innerHTML = '<td>Ville</td>';
+document.getElementById('sukunimi').innerHTML = '<td>Pellosniemi</td>';
+document.getElementById('lempinimi').innerHTML = '<td>Pelzi</td>';
+document.getElementById('osoite').innerHTML = '<td>Vanha maantie 6</td>';
+
