816a31831f31280547544202858799ce4c8d8056
VillePellosniemi <34025719+VillePellosniemi@users.noreply.github.com>
2018-10-31 20:49:33 +0200

    update

--- js5/t3/teht3.js
+++ js5/t3/teht3.js
@@ -6,10 +6,10 @@ const kuvat = [
   'http://placekitten.com/331/240',
 ];
 
+let listanpituus = kuvat.length;
 let i;
-for (i=0; i<kuvat.length; i++) {
-  let ul = document.getElementById('kuva');
-  let kuva = document.createElement('img');
-  let k = document.getElementsByTagName('img');
-  k.innerHTML = kuvat[i];
+
+for (i=0; i<listanpituus; i++) {
+  let lista = document.getElementById('kuva').innerHTML = '<li class="lista"></li>';
+  lista.document.getElementsByClassName('lista').innerHTML = kuvat[i];
 }
