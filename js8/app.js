212837ee7fae09bb5003acd1e90c299da1492a41
Ville Pellosniemi <villeope@metropolia.fi>
2018-11-08 11:44:22 +0200

    update

--- js8/app.js
+++ js8/app.js
@@ -1,6 +1,19 @@
 const nappi = document.querySelector('#hakunappi');
 nappi.addEventListener('click', Haku);
 
-function Haku (evt) {
+const vastaus = document.getElementById('hakuteksti').value;
+
+function Haku () {
+  fetch('http://api.tvmaze.com/search/shows?q=girls')
+  .then(function(vastaus) {
+    return vastaus.json();
+  }).then(function(json){
+    Tulos(json);
+  }).catch(function(error){
+    console.log(error);
+  });
+}
+
+function Tulos () {
 
 }
