d6e5e75187c7c63254062dd060817425dd165067
VillePellosniemi <34025719+VillePellosniemi@users.noreply.github.com>
2019-01-29 22:46:00 +0200

    update

--- Javascript-jakso-3/ownPage/app.js
+++ Javascript-jakso-3/ownPage/app.js
@@ -1,1 +1,21 @@
 'use strict';
+
+//balloon
+
+let balloon = document.querySelector("#balloon");
+
+window.addEventListener("keydown", event => {
+  if (event.key === "ArrowUp") {
+    let style = window.getComputedStyle(balloon, null).getPropertyValue('font-size');
+    let currentSize = parseFloat(style);
+    balloon.style.fontSize = (currentSize + 10) + 'px';
+    event.preventDefault();
+  } else if (event.key === "ArrowDown") {
+    let style = window.getComputedStyle(balloon, null).getPropertyValue('font-size');
+    let currentSize = parseFloat(style);
+    balloon.style.fontSize = (currentSize - 10) + 'px';
+    event.preventDefault();
+  }
+});
+
+if (balloon) 
