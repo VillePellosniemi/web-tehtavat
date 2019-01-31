0f88605db12086b5d0b1c5144a98d9601db13a41
VillePellosniemi <34025719+VillePellosniemi@users.noreply.github.com>
2019-01-31 17:14:38 +0200

    update

---
+++ Javascript-jakso-3/ownPage/parallax.js
@@ -0,0 +1,14 @@
+window.addEventListener("mousemove", Move);
+let layer = document.getElementById('layerone');
+
+function Move (e) {
+  let x = e.clientX;
+  let y = e.clientY;
+  let centerX = x - 630;
+  let centerY = y - 306;
+  let coor = "Coordinates: (" + x + "," + y + ")";
+  let coorCenter = "Coordinates center: (" + centerX + "," + centerY + ")";
+  document.getElementById("coordinates").innerHTML = coor;
+  document.getElementById("coordinatesCenter").innerHTML = coorCenter;
+  layer.style.transform = 'translateX(' + x/10 + 'px) translateY(' + y/100 + 'px)';
+}
